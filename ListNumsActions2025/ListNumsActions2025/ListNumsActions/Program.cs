b135cacd924ac4f5f448664c56e6f9e168010d85
SSPanev <s.panev2020@pmg-pernik.com>
2025-11-06 12:19:25 +0200

    1

---
+++ ListNumsActions2025/ListNumsActions2025/ListNumsActions/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ListNumsActions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string[] cmd = Console.ReadLine().Split().ToArray();
+                string command = cmd[0];
+                if (command.ToLower() == "finish")
+                {
+                    break;
+                }
+                switch (command)
+                {
+                    case "ins": int index= int.Parse(cmd[1]);
+                        int num = int.Parse(cmd[2]);
+                        nums.Insert(index,num);
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(" ", nums));
+                        break;
+                    case "contains":
+                        
+                        //TODO
+                        break;
+
+
+                    //TO DO
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
